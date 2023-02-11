ee694fc8dcd5bed1dee99e87e87676ca15649101
reemansour <mansourreem02gmail.com>
2023-02-11 18:18:56 +0200

    Buttons Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'homepage.dart';
+import 'menu.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         initialRoute: 'homepage',
         routes: {
-          'homepage': (context) => const HomePage(),
+          'homepage': (context) => const MyScreen(),
         });
   }
 }
---
+++ lib/widgets/main_buttons.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../homepage.dart';
+
+// ignore: must_be_immutable
+class MainButtons extends StatelessWidget {
+  String text;
+  IconData icon;
+  MainButtons({
+    Key? key,
+    required this.text,
+    required this.icon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      label: Text(
+        text,
+        style: const TextStyle(color: Colors.black, fontSize: 20),
+      ),
+      backgroundColor: const Color.fromARGB(255, 243, 199, 148),
+      icon: Icon(
+        icon,
+        size: 24.0,
+        color: Colors.black,
+      ),
+      onPressed: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      },
+    );
+  }
+}
