25f9f1411804db18dc8fe887629f6c4f4de8c705
basmadessouky <basma1900957@miuegypt.edu.eg>
2023-02-20 22:51:08 +0200

    Fixed an error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'menu.dart';
+import 'package:kemet/homepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         initialRoute: 'homepage',
         routes: {
-          'homepage': (context) => const MyScreen(),
+          'homepage': (context) => const HomePage(),
         });
   }
 }
